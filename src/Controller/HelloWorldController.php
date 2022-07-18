aceaf0efa70f76e5a8556882766ccf4ccbb7f9d9
Pierre Bretéché <pbreteche@dawan.fr>
2022-07-18 15:04:34 +0200

    Accès aux objets Request et Response

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +17,21 @@ class HelloWorldController extends AbstractController
         return $this->render('hello_world/index.html.twig', [
             'test_twig_variable' => 'Salut tout le monde',
         ]);
+    }
+
+    /**
+     * @Route("/hello/request", name="app_hello_request")
+     */
+    public function index2(Request $request): Response
+    {
+        dump($request->getUri());
+
+        $response = new Response('ceci est du texte plat', Response::HTTP_OK, [
+            'Content-type' => 'text/plain',
+        ]);
+
+        $response->headers->set('Content-type', 'text/html');
+
+        return $response;
     }
 }
