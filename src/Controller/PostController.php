f86e75785374f45665346dd39db481f1ffe98c58
Pierre Bretéché <pbreteche@dawan.fr>
2022-07-19 10:10:51 +0200

    Atelier pagination

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,21 +4,32 @@ namespace App\Controller;
 
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
+    const POST_LIMIT = 2;
+
     /**
      * @Route("/post")
      */
-    public function index(PostRepository $postRepository): Response
+    public function index(Request $request, PostRepository $postRepository): Response
     {
         $postsCount = $postRepository->count([]);
-        $posts = $postRepository->findBy([], ['createdAt' => 'DESC'], 10);
+        $maxPage = ceil($postsCount / self::POST_LIMIT);
+        $pageNumber = $request->query->get('p', 1);
+        if (0 > $pageNumber || $maxPage < $pageNumber) {
+            $pageNumber = 1;
+        }
+        $offset = ($pageNumber - 1) * self::POST_LIMIT;
+        $posts = $postRepository->findBy([], ['createdAt' => 'DESC'], self::POST_LIMIT, $offset);
 
         return $this->render('post/index.html.twig', [
             'posts' => $posts,
+            'max' => $maxPage,
+            'page' => $pageNumber,
         ]);
     }
 }
