babc591bc72a39ade639b4f14303ef90d3fb5846
Pierre Bretéché <pbreteche@dawan.fr>
2022-07-22 14:59:42 +0200

    Configuration de deux connections base de données

---
+++ src/EntityExtra/Profile.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\EntityExtra;
+
+use App\Entity\User;
+use App\Repository\ProfileRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProfileRepository::class)
+ */
+class Profile
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column
+     */
+    private $lastName;
+
+    /**
+     * @var User
+     */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+}
